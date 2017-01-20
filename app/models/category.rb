81dc3360dbfe7194172fd824f9995ec6fc5769e7
ThomasDebek <tom.debek@gmail.com>
2017-01-20 20:37:42 +0100

    Czesciowa konfiguracja modeli

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,6 @@
 class Category < ActiveRecord::Base
+  has_many :prodcuts
+
+  validates :name, presence: true      # zrobmy odrazu walidacje na to pole
+
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,13 @@
 class Product < ActiveRecord::Base
   belongs_to :category
+
+  validates :name, presence: true                        # pole z nazwa nie moze byc puste
+  validates :description, presence: true                 # pole z opisem nie moze byc puste
+  validates :price, numericality: { greater_than: 0.0 }  # cena powinna byc wieksza niz zero i musi byc liczba
+  validates :category, presence: true                    # deklarujemy ze chcemy ja zwalidowac i przypozadkowac do kategorii
+
+
+  mount_uploader :photo, ProductPhotoUploader         # Musimy takze zamontowac nasz uploader pochodzacy z gemu CarrierWave
+                                                      # Jako argument przkazujemy nazwe photo, ktora bedzie przechowywala sciezke do pliku
+                                                      # Oraz nazwe klasy, ktora obsluguje dane uploadsy
 end
