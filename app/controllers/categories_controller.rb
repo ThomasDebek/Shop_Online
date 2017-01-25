2b8554261429b455e9f424044dd88cbf945d8ec6
ThomasDebek <tom.debek@gmail.com>
2017-01-25 20:32:09 +0100

    Wyswielmy prawdziwe dane w pasku bocznym naszeg layoutu, w kontrolerze , a takze w widokach

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,6 @@
 class CategoriesController < ApplicationController
   def show
+    @category = Category.find(params[:id])                        # i w akcji show powininsmy znalesc kategorie ktora chcemy wysietlac
+    @products = @category.products.page(params[:page]).per(6)     # a po drugie znalezc produkty z tej kategorii i dodamy paginacje, na kazdej stronie 6produktow
   end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ActiveRecord::Base
-  has_many :prodcuts
+  has_many :products
 
   validates :name, presence: true      # zrobmy odrazu walidacje na to pole
 
