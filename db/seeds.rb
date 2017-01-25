5a5bda3c14be03f8aac50e4c779038f689a6dec4
ThomasDebek <tom.debek@gmail.com>
2017-01-25 19:43:09 +0100

    Skrypt generujacy loswe dane i zapisujacy do db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+
+
+8.times do                                                   # W tym bloku losujemy 8 kategori
+  Category.create! name: Faker::Hacker.ingverb.capitalize    # Tu tworzymy nowe kategorie z losowa nazwa wygenerowana przez Fakera
+end
+
+category_ids = Category.pluck(:id)                           # Tutaj przechowuje w zmiennej wszystkie numery id tych kategorii,
+                                                             # metoda pluck  w active record przyjmuje 1argument w postaci nazwy kolumny
+                                                             # i ta metoda zwraca tablice z wartosciami w tej kolumnie wszystkich wierszy w bazie dannych
+
+print "Product "
+200.times do       # nastepnie 200 razy wywolujemy ten blok, i w nim dodajemy produkty
+  product = Product.create! name: "#{Faker::Hacker.verb} #{Faker::Hacker.noun}".capitalize, # dajemy naze
+                            description: Faker::Hacker.say_something_smart,                 # wywolujemy krotki opis
+                            long_description: Faker::Lorem.paragraphs(3).join("\n\n"),      # Wywolujemy dlugi opis
+                            category_id: category_ids.sample,                               # wybieramy losowa kategorie, czyli na tablicy z wszystkimi identyfikatorami kategorii wywolujemy metode sample, ktroy zwraca losowy element z tej tablicy
+                            price: Faker::Number.decimal(3, 2)                              # generujemy losowa cene
+  product.remote_photo_url = Faker::Avatar.image(                   # Generujemy losowy obrazek produktu
+      product.name.parameterize,
+      "640x480",                  # ma miec rozmiar
+      "jpg",                      # byc w formacie jpg
+      "set#{[1, 2, 3].sample}",   # obrazek ma byc wykorzystany ze wszystkich setow 1-set wyswietla roboty, 2-set twarze robotow, 3-set potworki
+      "bg#{[1, 2].sample}"        # tutaj mowimy z ktorego tla losoego bedziemy korzystac
+  )
+  product.save   # zapisujemy produkt
+  print "."      # kropecza sygnalizuje nam ze cos sie dzieje poniewaz skrytp bedzie dzialal dosc dlugo, ze sie nie zawiesil, nie zapetlil
+end
+puts             # i wyswitlamy
+
