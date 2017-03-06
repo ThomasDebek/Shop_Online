f0617bc0520f521df427f8d0c3013f694a00d18c
ThomasDebek <tom.debek@gmail.com>
2017-03-06 17:41:52 +0100

    Zabezpiczenie kontrollera Admina, zmiana klasy bazowej

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,4 +1,4 @@
-class Admin::OrdersController < ApplicationController
+class Admin::OrdersController < Admin::BaseController       # Musimy zmienic klase bazowa - dzieki czemu zabezpieczamy sie haslem
   def index
     @orders = Order.not_in_state(:new).page(params[:page]).per(20)   # uzyjemy metody od Statesmana -  i wymieniamy w ktorych stanach me tej metody nie byc, nie chcemy jej wylacznie w stanie new, i oczywiscie paginacja, i chce miec 20 zamowien na kazdej stronie
   end
